45cd847ca2ace68688b27858d256a0d55b1d3d1d
Jonathan De Jesus <jonathand9295@gmail.com>
2019-06-26 23:47:03 -0500

    create guard and an components for login and dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { AppRouting } from './app.routing';
+import { LoginModule } from './login/login.module';
+import { DashboardModule } from './dashboard/dashboard.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRouting,
+    LoginModule,
+    DashboardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.routing.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { DashboardHomeComponent } from './dashboard/dashboard-home/dashboard-home.component';
+import { LoginGuard } from './login.guard';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { LoginFormComponent } from './login/login-form/login-form.component';
+
+const routes: Routes = [
+  {path: '', component: LoginFormComponent},
+  {path: 'login', component: LoginFormComponent},
+  {path: 'dashboard', component: DashboardHomeComponent, canActivate: [LoginGuard]},
+  { path: '**', component: NotFoundComponent },
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+  declarations: [],
+})
+export class AppRouting { }
